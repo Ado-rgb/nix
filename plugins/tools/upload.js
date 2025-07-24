84d5f455d5a38ec0f78f70c0f9d2e2ca43e0c1c2
Ado <minexdt@gmail.com>
2025-07-24 16:48:27 -0600

    Actualizar upload.js

--- plugins/tools/upload.js
+++ plugins/tools/upload.js
@@ -2,24 +2,17 @@ exports.default = {
    names: ['Tools'],
    tags: ['tourl', 'upload'],
    command: ['tourl', 'upload'],
-   start: async (m, {
-      conn,
-      prefix,
-      command,
-      mime,
-      quoted,
-      Format
-   }) => {
+   start: async (m, { conn, prefix, command, mime, quoted, Format }) => {
       if (/image|video|audio|webp/.test(mime)) {
-         m.reply(loading)
+         m.reply('⏳ Subiendo tu archivo...');
          const result = await Format.upload2(await conn.download(quoted));
-         const caption = `Upload Berhasil √ \n${result}`
+         const caption = `✅ Subida exitosa\n${result}`;
          return conn.adReply(m.chat, caption, cover, m);
       } else {
-         return m.reply(`Balas Media Atau Kirim Media Dengan Caption ${prefix}upload atau ${prefix}tourl`);
+         return m.reply(`⚠️ Responde a un archivo multimedia o envía uno con el comando ${prefix}upload o ${prefix}tourl`);
       }
    },
-   limit: 3,
+   limit: false,
    register: false,
    premium: false
-}
+};
