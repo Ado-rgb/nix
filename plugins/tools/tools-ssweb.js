a74fcd048e3b9bf87b2fa23c75ba5317f88e30b9
Ado <minexdt@gmail.com>
2025-07-24 17:10:00 -0600

    Actualizar tools-ssweb.js

--- plugins/tools/tools-ssweb.js
+++ plugins/tools/tools-ssweb.js
@@ -1,15 +1,20 @@
 exports.default = {
    names: ['Tools'],
    tags: ['ssweb'],
-   command: ['ssweb'],
-   start: async (m, {
-      conn,
-      text,
-      prefix,
-      command     
-   }) => {
-      if (!text) return m.reply('Masukan link atau url yang mau di screenshot webnya\ncontoh: ' + prefix + command + ' ' + link_group);
-      await conn.sendFile(m.chat, await BUFFER_URL('https://image.thum.io/get/width/1900/crop/1000/fullpage/' + text), text, m);
+   command: ['ssweb', 'ss'],
+   start: async (m, { conn, text, prefix, command }) => {
+      if (!text) return m.reply(
+         `⚠️ Debes ingresar un enlace para hacer la captura.\n` +
+         `Ejemplo: ${prefix + command} https://ejemplo.com`
+      );
+
+      await m.reply('⏳ Capturando la página, espera un momento...');
+      await conn.sendFile(
+         m.chat,
+         await BUFFER_URL(`https://image.thum.io/get/width/1900/crop/1000/fullpage/${text}`),
+         text,
+         m
+      );
    },
-   limit: 2
-}
+   limit: false
+};
