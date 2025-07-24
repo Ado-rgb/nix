5a062cdbfd9a85e5aebdf94cd577cf0c196a674d
Ado <minexdt@gmail.com>
2025-07-24 17:28:05 -0600

    Actualizar group-kick.js

--- plugins/group/group-kick.js
+++ plugins/group/group-kick.js
@@ -1,20 +1,20 @@
 exports.default = {
    names: ['Group Menu'],
-   tags: ['kick', 'tendang'],
-   command: ['kick', 'tendang', '-', 'dor', 'door'],
-   start: async (m, {
-      conn, 
-      text
-   }) => {
+   tags: ['expulsar'],
+   command: ['kick', 'expulsar', '-', 'sacar', 'fuera'],
+   start: async (m, { conn, text }) => {
       if (m?.quoted) {
+         // Expulsar a la persona de un mensaje citado
          await conn.groupParticipantsUpdate(m.chat, [m.quoted.sender], "remove");
-         return m.reply(`Berhasil Menghapus @${m.quoted.sender.split("@")[0]} Dari Grup Ini`, { mentions: [m.quoted.sender] });
+         return m.reply(`Se ha expulsado a @${m.quoted.sender.split("@")[0]} del grupo.`, { mentions: [m.quoted.sender] });
       } else if (text) {
+         // Expulsar a la persona etiquetada
          const user = conn.parseMention(text);
+         if (!user.length) return m.reply('Debes etiquetar a alguien para expulsarlo.');
          await conn.groupParticipantsUpdate(m.chat, [...user], "remove");
-         return m.reply(`Berhasil Menghapus @${user[0].split("@")[0]} Dari Grup Ini`, { mentions: [...user] });
+         return m.reply(`Se ha expulsado a @${user[0].split("@")[0]} del grupo.`, { mentions: [...user] });
       } else {
-         return m.reply(`Tag atau Balas Orangnya Yang Mau Di Keluarkan`);
+         return m.reply('Etiqueta o responde al mensaje de la persona que quieras expulsar.');
       }
    },
    group: true,
