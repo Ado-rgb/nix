24c1e9e395e2fa7f3ac5bfc57615beb6c7d4f300
Ado <minexdt@gmail.com>
2025-07-24 16:20:31 -0600

    Actualizar owner-addlimit.js

--- plugins/owner/owner-addlimit.js
+++ plugins/owner/owner-addlimit.js
@@ -1,20 +1,39 @@
 exports.default = {
    names: ['Owner'],
    tags: ['addlimit'],
-   command: ['addlimit', 'tambahlimit'],
+   command: ['addlimit', 'añadirlímite'],
    start: async (m, {
       text,
       prefix,
       command
    }) => {
-      if (!text) return m.reply(`Masukkan/ tag nomor dan limitnya \natau tag yang mau di tambahkan limit dan masukan limitnya \nContoh: ${prefix + command} nomor limit\nContoh: ${prefix + command} 62xxxxx 25 \nAtau\nContoh: ${prefix + command} @tag 25`);
+      if (!text) return m.reply(
+`⚠️ *Uso del comando:*
+Ingresa o etiqueta el número y el límite que deseas agregar.
+
+Ejemplos:
+> ${prefix + command} número límite
+> ${prefix + command} 521xxxxxxxxxx 25
+> ${prefix + command} @usuario 25`
+      );
+
       const parts = text.split(" "), limit = parts.pop(), number = parts.join(" "); 
-      if (!limit || isNaN(limit)) return m.reply(`Masukan limitnya berupa angka \nContoh: ${prefix + command} nomor(spasi)limit\nContoh: ${prefix + command} 62xxxxx 25\nAtau \nContoh: ${prefix + command} @tag 25`);
+      if (!limit || isNaN(limit)) return m.reply(
+`⚠️ *Error:* El límite debe ser un número válido.
+
+Ejemplos:
+> ${prefix + command} número límite
+> ${prefix + command} 521xxxxxxxxxx 25
+> ${prefix + command} @usuario 25`
+      );
+
       const Number = number.replace(/[@+\s-]/g, '');
       const num = `${Number}@s.whatsapp.net`;
-      if (!db.users[num]) return m.reply(`Pengguna dengan nomor ${num} tidak ditemukan dalam database. Pastikan nomor sudah terdaftar.`);
+
+      if (!db.users[num]) return m.reply(`❌ Usuario con número *${num}* no encontrado en la base de datos. Asegúrate de que esté registrado.`);
+
       db.users[num].limit += parseInt(limit);
-      m.reply(`Berhasil Menambahkan ${limit} Limit Ke ${num.split('@')[0]}`);
+      m.reply(`✅ Se añadieron *${limit}* límites a *${num.split('@')[0]}* 🎉`);
    },
    owner: true
 };
